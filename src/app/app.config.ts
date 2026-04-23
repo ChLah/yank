d25e0c68278e23e40b380e82050ec3a1f5e1125a
Christoph Lahner <clahner92@gmail.com>
2026-04-23 14:17:51 +0200

    feat(i18n): wire TranslateModule and APP_INITIALIZER in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { routes } from './app.routes';
+import { TypescriptTranslateLoader } from './i18n/translate-loader';
+import { I18nService } from './core/services/i18n.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withHashLocation()),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useClass: TypescriptTranslateLoader,
+        },
+      }),
+    ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (i18nService: I18nService) => () => i18nService.init(),
+      deps: [I18nService],
+      multi: true,
+    },
   ],
 };
