007d29c6e8362a0441d8f706f22f84630f1b6b0e
Christoph Lahner <clahner92@gmail.com>
2026-04-27 12:11:21 +0200

    feat(settings): add SettingCheckboxComponent

---
+++ src/app/features/settings/components/setting-checkbox/setting-checkbox.component.spec.ts
@@ -0,0 +1,7 @@
+import { SettingCheckboxComponent } from './setting-checkbox.component';
+
+describe('SettingCheckboxComponent', () => {
+  it('is defined', () => {
+    expect(SettingCheckboxComponent).toBeDefined();
+  });
+});
